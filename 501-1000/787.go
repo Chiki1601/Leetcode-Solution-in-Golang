1b5d16472b1646fef80809693348afb040c5dac4
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2024-02-23 12:19:16 +0530

    Update 787.go

--- 501-1000/787.go
+++ 501-1000/787.go
@@ -1,40 +1,19 @@
-type Path struct {
-    city  int
-    price int
-    k     int
-}
-
-type minHeap []Path
-func (h minHeap) Len() int { return len(h) }
-func (h minHeap) Less(i, j int) bool { return h[i].price < h[j].price }
-func (h minHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
-func (h *minHeap) Push(x interface{}) { *h = append(*h, x.(Path)) }
-func (h *minHeap) Pop() interface{} {
-    tmp := (*h)[len(*h) - 1]
-    *h = (*h)[:len(*h) - 1]
-    return tmp
-}
-
 func findCheapestPrice(n int, flights [][]int, src int, dst int, K int) int {
-    cache := make(map[int][][]int)
-    for _, v := range flights {
-        if _, ok := cache[v[0]]; ok {
-            cache[v[0]] = append(cache[v[0]], []int{v[1], v[2]})
-        } else {
-            cache[v[0]] = [][]int{[]int{v[1], v[2]}}
+    dp, tmp := make([][]int, K + 2), 1 << 31
+    for i := range dp { dp[i] = make([]int, n) }
+    for i := 0; i < K + 2; i++ { for j := 0; j < n; j++ { dp[i][j] = tmp } }
+    dp[0][src] = 0
+    for i := 1; i < K + 2; i++ {
+        dp[i][src] = 0
+        for _, flight := range flights {
+            dp[i][flight[1]] = min(dp[i][flight[1]], dp[i - 1][flight[0]] + flight[2])
         }
     }
-    h := &minHeap{}
-    heap.Init(h)
-    heap.Push(h, Path{src, 0, K + 1})
-    for h.Len() > 0 {
-        path := heap.Pop(h).(Path)
-        if path.city == dst { return path.price }
-        if path.k > 0 {
-            for _, dstAndPrice := range cache[path.city] {
-                heap.Push(h, Path{dstAndPrice[0], path.price + dstAndPrice[1], path.k - 1})
-            }
-        }
-    }
+    if dp[K + 1][dst] < tmp { return dp[K + 1][dst] }
     return -1
+}
+
+func min(a, b int) int {
+    if a < b { return a }
+    return b
 }
